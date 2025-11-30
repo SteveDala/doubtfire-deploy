a11edabdc19b52628096b3c5e966d5bccd99b560
Steven Dalamaras <sd@stevendalamaras.dev>
2025-11-30 14:03:43 +1000

    fix: follow 10.0.x devcontainer approach
    I have changed the 9.x devcontainer build approach to more
    closely mirror 10.0.x. The use case is for having multiple
    development branches cloned and wanting to rapidly switch between
    the two for testing, without having the two separate branches
    conflict by sharing databases, node modules, and ruby gems.

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -6,7 +6,8 @@ ln -s /workspace/.devcontainer/.p10k.zsh $HOME
 ln -s /workspace/.devcontainer/.irbrc $HOME
 
 
-sudo chown vscode:vscode /var/lib/mysql
+# sudo chown vscode:vscode /var/lib/mysql
+sudo chown vscode:vscode /var/run/docker.sock
 # sudo chmod a+rw /var/lib/mysql
 sudo chmod a+rw /workspace/tmp
 sudo chmod a+rw /workspace/node_modules
--- .devcontainer/setup_formatif_db.sh
+++ .devcontainer/setup_formatif_db.sh
@@ -1,61 +1,61 @@
 #!/bin/bash
 
-# Wait for mysql to start
-check_process() {
-  [ "$1" = "" ] && return 0
-  [ $(pgrep -n $1) ] && return 1 || return 0
-}
-
-check_process "mysqld"
-CHECK_RET=$?
-if [ $CHECK_RET -eq 0 ]; then # none exist
-  echo 'Waiting for mysqld to start'
-  sleep 5
-fi
-
-check_process "mysqld"
-CHECK_RET=$?
-if [ $CHECK_RET -eq 0 ]; then # none exist
-  echo 'Failed to find mysqld - please make sure the database is running'
-  exit 1
-fi
-
-NEXT_WAIT_TIME=0
-# Loop until mysqld is ready...
-while true; do
-  RESP=`mysql --user=root --password="${MYSQL_ROOT_PASSWORD}" -qfsBNe "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='${MYSQL_DATABASE}';" 2>>/dev/null`
-
-  if [ $? -ne 0 ]; then
-    if [ $NEXT_WAIT_TIME -eq 0 ]; then
-      echo -n "Waiting for access to mysql..."
-    fi
-
-    # Check mysqld is still running...
-    check_process "mysqld"
-    CHECK_RET=$?
-    if [ $CHECK_RET -eq 0 ]; then # none exist
-      echo "x"
-      echo "Mysqld has stopped... unable to check and create database"
-      exit 1
-    fi
-
-    if [ $NEXT_WAIT_TIME -ge 10 ]; then
-      echo "x"
-      echo "Unable to connect to mysql after $NEXT_WAIT_TIME retries"
-      exit 1
-    fi
-
-    echo -n "."
-    sleep $(( NEXT_WAIT_TIME++ ))
-  else
-    echo "!"
-    break
-  fi
-done
-
-if [[ $RESP -gt 5 ]]; then
-  echo "Database already exists"
-else
+# # Wait for mysql to start
+# check_process() {
+#   [ "$1" = "" ] && return 0
+#   [ $(pgrep -n $1) ] && return 1 || return 0
+# }
+#
+# check_process "mysqld"
+# CHECK_RET=$?
+# if [ $CHECK_RET -eq 0 ]; then # none exist
+#   echo 'Waiting for mysqld to start'
+#   sleep 5
+# fi
+#
+# check_process "mysqld"
+# CHECK_RET=$?
+# if [ $CHECK_RET -eq 0 ]; then # none exist
+#   echo 'Failed to find mysqld - please make sure the database is running'
+#   exit 1
+# fi
+#
+# NEXT_WAIT_TIME=0
+# # Loop until mysqld is ready...
+# while true; do
+#   RESP=`mysql --user=root --password="${MYSQL_ROOT_PASSWORD}" -qfsBNe "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='${MYSQL_DATABASE}';" 2>>/dev/null`
+#
+#   if [ $? -ne 0 ]; then
+#     if [ $NEXT_WAIT_TIME -eq 0 ]; then
+#       echo -n "Waiting for access to mysql..."
+#     fi
+#
+#     # Check mysqld is still running...
+#     check_process "mysqld"
+#     CHECK_RET=$?
+#     if [ $CHECK_RET -eq 0 ]; then # none exist
+#       echo "x"
+#       echo "Mysqld has stopped... unable to check and create database"
+#       exit 1
+#     fi
+#
+#     if [ $NEXT_WAIT_TIME -ge 10 ]; then
+#       echo "x"
+#       echo "Unable to connect to mysql after $NEXT_WAIT_TIME retries"
+#       exit 1
+#     fi
+#
+#     echo -n "."
+#     sleep $(( NEXT_WAIT_TIME++ ))
+#   else
+#     echo "!"
+#     break
+#   fi
+# done
+#
+# if [[ $RESP -gt 5 ]]; then
+#   echo "Database already exists"
+# else
   echo "Creating and populating database - do not shutdown!"
   cd /workspace/doubtfire-api
 
@@ -69,4 +69,4 @@ else
   rm /workspace/tmp/database_populate.log
 
   echo "Simulation of signoff complete"
-fi
+# fi
